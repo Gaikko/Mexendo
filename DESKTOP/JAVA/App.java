77a74b1b079c76a5c59c9219268fd7f57dc5678b
Gaikko Alves <Gaikko@gmail.com>
2022-05-06 11:34:12 -0400

    Scanner

--- DESKTOP/JAVA/App.java
+++ DESKTOP/JAVA/App.java
@@ -1,10 +1,16 @@
-import Calculadora;
+import java.util.Scanner;
 public class App{
     public static void main(String[] args) {
         System.out.println("Olá Mundo!");
         System.out.println("Somar!");
         Calculadora calc = new Calculadora();
-        double soma = calc.somar(10, 20);
-        System.out.println("A Soma de 10 + 20 = " + soma);
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Digite o Primeiro Número: ");
+        double n1 = scn.nextDouble();
+        System.out.println("Digite o Segundo Número: ");
+        double n2 = scn.nextDouble();
+        double soma = calc.somar(n1, n2);
+        System.out.println("A Soma de " + n1 +" + "+ n2 +" = " + soma);
+        scn.close();
     }
 }
