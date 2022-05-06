c7ed1233e7e075a73dd579501cb50218ddc376fe
Gaikko Alves <Gaikko@gmail.com>
2022-05-06 16:42:46 -0400

    Exponenciar

--- DESKTOP/JAVA/App.java
+++ DESKTOP/JAVA/App.java
@@ -11,6 +11,14 @@ public class App{
         double n2 = scn.nextDouble();
         double soma = calc.somar(n1, n2);
         System.out.println("A Soma de " + n1 +" + "+ n2 +" = " + soma);
+        double sub = calc.subtrair(n1, n2);
+        System.out.println("A Subtração de " + n1 +" - "+ n2 +" = " + sub);
+        double divi = calc.dividir(n1, n2);
+        System.out.println("A Divisão de " + n1 +" / "+ n2 +" = " + divi);
+        double multi = calc.multiplicar(n1, n2);
+        System.out.println("A Multiplicação de " + n1 +" * "+ n2 +" = " + multi);
+        double expo = calc.exponenciar(n1, n2);
+        System.out.println("A Base de " + n1 +" elevado a Exponêcia de "+ n2 +" = " + expo);
         scn.close();
     }
 }
--- DESKTOP/JAVA/Calculadora.java
+++ DESKTOP/JAVA/Calculadora.java
@@ -2,4 +2,16 @@ public class Calculadora {
     double somar (double n1, double n2){
         return n1 + n2;
     }
+    double subtrair (double n1, double n2){
+        return n1 - n2;
+    }
+    double dividir (double n1, double n2){
+        return n1 / n2;
+    }
+    double multiplicar (double n1, double n2){
+        return n1 * n2;
+    }
+    double exponenciar (double n1, double n2){
+        return Math.pow(n1, n2);
+    }
 }
